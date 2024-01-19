f0130f2e7a1422c9628998773d1a095a4ee67b2e
José Gabriel Paludo <josegabrielpaludo@gmail.com>
2024-01-19 17:05:39 -0300

    new

--- todo-app/frontend/src/App.tsx
+++ todo-app/frontend/src/App.tsx
@@ -2,74 +2,153 @@ import React, { ChangeEventHandler, useState, useEffect } from "react";
 import NoteItem from "./components/NoteItem";
 import axios from "axios";
 
+type noteType = {
+  id: string;
+  title: string;
+  description?: string;
+};
+
 const App = () => {
 
-useEffect(() => {
-  
-})
 
-  const [notes, setNotes] = useState<{
-    id: string;
-    title: string;
-    description?: string;
-  }[]>([])
-
-const [values, setValues] = useState({
+  const [count, setCount] = useState(0);
+  const [noteToView, setNoteToView] = useState<noteType>();
+  const [notes, setNotes] = useState<noteType[]>([]);
+  const [values, setValues] = useState({
     title: "",
-    description: ""
-  })
+    description: "",
+  });
+  const [selectedNoteId, setSelectedNoteId] = useState("");
 
-const handleChange : ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = ({target}) => {
-  const {name, value} = target;
-  setValues({...values, [name]: value})
-}
+  const handleChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = ({ target }) => {
+    const { name, value } = target;
+    setValues({ ...values, [name]: value });
+  };
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const { data } = await axios("http://localhost:8000/note");
+      setNotes(data.notes);
+    };
+
+    fetchNotes();
+  }, []);
 
   return (
-    
+
     <div className="max-w-3xl mx-auto font-sans">
-      <form onSubmit={async (evt) =>{
-        evt.preventDefault();
-        const {data} = await axios.post("http://localhost:8000/note/create", {
-          title: values.title,
-          description: values.description
-        })
-        setNotes([data.note,...notes])
-        setValues({title: '', description: ''})
-      }} className=" bg-white shadow-md rounded p-5 space-y-6">
+      <form
+        onSubmit={async (evt) => {
+          evt.preventDefault();
+          if (selectedNoteId) {
+            const { data } = await axios.patch(
+              "http://localhost:8000/note/" + selectedNoteId,
+              {
+                title: values.title,
+                description: values.description,
+              }
+            );
+
+            const updatedNotes = notes.map((note) => {
+              if (note.id === selectedNoteId) {
+                note.title = data.note.title;
+                note.description = data.note.description;
+              }
+              return note;
+            });
+
+            setNotes([...updatedNotes]);
+            setValues({ title: "", description: "" });
+            return;
+          }
+
+          const { data } = await axios.post(
+            "http://localhost:8000/note/create",
+            {
+              title: values.title,
+              description: values.description,
+            }
+          );
+          setNotes([data.note, ...notes]);
+          setValues({ title: "", description: "" });
+        }}
+        className="space-y-6 bg-white shadow-md rounded p-5"
+      >
+
+        <div>
+          <span>{count} </span>
+          <button type="button" onClick={() => setCount(count + 1)}>Click Me</button>
+        </div>
         <h1 className="font-semibold text-2xl text-blue-400 text-center">Notes Application</h1>
         <div>
           {values.title.trim() && values.title.length < 2 ? (
-          <p className="text-red-500">Title is to short</p>
+            <p className="text-red-500">Title is to short</p>
           ) : null}
           <input className="w-full border-b-2 border-gray-700 outline-none"
             type="text"
             placeholder="Title"
-            onChange={handleChange} 
+            onChange={handleChange}
             value={values.title}
             name="title"
-            />
+          />
         </div>
         <div>
-          <textarea 
-          onChange={handleChange} 
-          value={values.description}
-          name="description"
-          className="w-full border-b-2 border-gray-700 outline-none resize-none h-36 " placeholder="Description"></textarea>
+          <textarea
+            onChange={handleChange}
+            value={values.description}
+            name="description"
+            className="w-full border-b-2 border-gray-700 outline-none resize-none h-36 " placeholder="Description"></textarea>
         </div>
         <div className="text-center">
-          <button 
-          onClick={()=>{
-            console.log(values)
-          }}
-          className="bg-blue-400 text-white px-5 py-2 rounded"
+          <button
+            onClick={() => {
+              console.log(values)
+            }}
+            className="bg-blue-400 text-white px-5 py-2 rounded"
           >Submit</button>
         </div>
       </form>
       {/* Note Items */}
-      {notes.map((note) => {
-        return <NoteItem key={note.title} title={note.title}/>
-      })}
-      
+      {notes.map((note) => (
+        <NoteItem
+        onViewClick={() => {
+          if (noteToView) {
+            setNoteToView(undefined);
+          } else {
+            setNoteToView(note);
+          }
+        }}
+        description={
+          noteToView?.id === note.id ? noteToView?.description : ""
+        }
+        onEditClick={() => {
+          setSelectedNoteId(note.id);
+          setValues({
+            title: note.title,
+            description: note.description || "",
+          });
+        }}
+        onDeleteClick={async () => {
+          const result = confirm("Are you sure?");
+          if (result) {
+            // delete
+            await axios.delete("http://localhost:8000/note/" + note.id);
+
+            // const updatedNotes = notes.filter(({ id }) => {
+            //   if (id !== note.id) return note;
+            // });
+            const updatedNotes = notes.filter(({ id }) => id !== note.id);
+
+            setNotes([...updatedNotes]);
+          }
+        }}
+        key={note.id}
+        title={note.title}
+      />
+      ))}
+
     </div>
   )
 }
--- todo-app/frontend/src/components/NoteItem.tsx
+++ todo-app/frontend/src/components/NoteItem.tsx
@@ -4,20 +4,30 @@ import React from 'react';
 import AppButton from './AppButton';
 
 interface Props {
-    title?: string,
-    text?: string
-}
-
-const NoteItem: FC<Props> = ({ title }) => {
+    title?: string;
+    description?: string;
+    onEditClick?(): void;
+    onDeleteClick?(): void;
+    onViewClick?(): void;
+  }
+  
+  const NoteItem: FC<Props> = ({
+    title,
+    description,
+    onEditClick,
+    onDeleteClick,
+    onViewClick,
+  }) => {
+  
     return (
         <div className="bg-white shadow-md rounded p-5 m-10 block">
             <div className="flex items-center justify-between">
                 {/* Use the title prop here */}
                 <p className="font-semibold text-gray-400 text-lg">{title}</p>
                 <div className="flex space-x-4">
-                    <AppButton color="green-500" icon="eye" onClick={()=>{}} />
-                    <AppButton color="gray-400" icon="edit" onClick={()=>{}} />
-                    <AppButton color="red-500" icon="trash" onClick={()=>{}} />
+                    <AppButton color="green-500" icon="eye" onClick={onViewClick} />
+                    <AppButton color="gray-400" icon="edit" onClick={onEditClick} />
+                    <AppButton color="red-500" icon="trash" onClick={onDeleteClick} />
 
                 </div>
             </div>
